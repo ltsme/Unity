e69b6c78a47b1c8df81060d4abe1e567df5f8da3
ltsme <guswns2415@naver.com>
2021-08-26 21:12:37 +0900

    08/26
    commit

---
+++ 3_Roulette/Assets/RouletteController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            this.rotSpeed = 5;
+        }
+
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        this.rotSpeed *= 0.995f;
+
+    }
+}
---
+++ 4_SwipeCar/Assets/CarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.startPos = Input.mousePosition; // 마우스를 클릭한 좌표
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition; // 마우스를 뗀 좌표
+
+            float swipeLength = (endPos.x - this.startPos.x);
+            this.speed = swipeLength / 1000.0f; // 감속
+
+            GetComponent<AudioSource>().Play();
+
+
+        }
+
+        transform.Translate(this.speed, 0, 0); // 이동, 로컬 좌표계
+        this.speed *= 0.98f; // 감속
+    }
+}
+
+
